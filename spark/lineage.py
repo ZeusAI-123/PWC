722065d4c3c1bafe18fb032f251e249d9cca336f
ZeusAI-123 <ztechtgit@gmail.com>
2026-01-13 19:28:16 +0530

    Update lineage.py

--- spark/lineage.py
+++ spark/lineage.py
@@ -13,3 +13,18 @@ def get_impacted_views_snowflake(conn, database, schema, table):
       AND referencing_object_domain IN ('VIEW', 'MATERIALIZED VIEW')
     """
     return pd.read_sql(query, conn)
+    
+def get_view_definitions(conn, database, schema, view_names):
+    views_list = ",".join([f"'{v}'" for v in view_names])
+
+    query = f"""
+    SELECT
+        table_schema,
+        table_name AS view_name,
+        view_definition
+    FROM {database}.INFORMATION_SCHEMA.VIEWS
+    WHERE table_schema = '{schema}'
+      AND table_name IN ({views_list})
+    """
+
+    return pd.read_sql(query, conn)
