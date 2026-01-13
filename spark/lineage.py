ae340f0a3dae70e1fc867fd14ddcf99a6d375461
ZeusAI-123 <ztechtgit@gmail.com>
2026-01-13 15:54:29 +0530

    Update lineage.py

--- spark/lineage.py
+++ spark/lineage.py
@@ -1,14 +1,15 @@
 import pandas as pd
 
-def get_impacted_views(conn, database, schema, table):
+def get_impacted_views_snowflake(conn, database, schema, table):
     query = f"""
     SELECT
-        referencing_object_schema AS view_schema,
-        referencing_object_name   AS view_name,
+        referencing_schema,
+        referencing_object_name AS view_name,
         referencing_object_domain AS object_type
-    FROM {database}.INFORMATION_SCHEMA.OBJECT_DEPENDENCIES
-    WHERE referenced_object_schema = '{schema}'
-      AND referenced_object_name   = '{table}'
+    FROM SNOWFLAKE.ACCOUNT_USAGE.OBJECT_DEPENDENCIES
+    WHERE referenced_database = '{database}'
+      AND referenced_schema   = '{schema}'
+      AND referenced_object_name = '{table}'
       AND referencing_object_domain IN ('VIEW', 'MATERIALIZED VIEW')
     """
     return pd.read_sql(query, conn)
