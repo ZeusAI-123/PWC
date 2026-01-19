2f9a81abcf2e0bb601bb2cb87c55451ef13d39e6
ZeusAI-123 <ztechtgit@gmail.com>
2026-01-19 21:40:45 +0530

    Create mongodb.py

---
+++ datasources/mongodb.py
@@ -0,0 +1,19 @@
+from pymongo import MongoClient
+from pymongo.errors import CollectionInvalid
+
+def get_mongodb_connection(
+    host,
+    port,
+    database,
+    username,
+    password
+):
+    client = MongoClient(
+        host=host,
+        port=port,
+        username=username,
+        password=password,
+        authSource=database
+    )
+
+    return client[database]
