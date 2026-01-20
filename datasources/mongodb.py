dc522eaf2f9fdaa0cd6eb373fd3285494e96a66f
ZeusAI-123 <ztechtgit@gmail.com>
2026-01-20 17:12:29 +0530

    Update mongodb.py

--- datasources/mongodb.py
+++ datasources/mongodb.py
@@ -1,19 +1,25 @@
+# from pymongo import MongoClient
+# from pymongo.errors import CollectionInvalid
+
+# def get_mongodb_connection(
+#     host,
+#     port,
+#     database,
+#     username,
+#     password
+# ):
+#     client = MongoClient(
+#         host=host,
+#         port=port,
+#         username=username,
+#         password=password,
+#         authSource=database
+#     )
+
+#     return client[database]
 from pymongo import MongoClient
-from pymongo.errors import CollectionInvalid
 
-def get_mongodb_connection(
-    host,
-    port,
-    database,
-    username,
-    password
-):
-    client = MongoClient(
-        host=host,
-        port=port,
-        username=username,
-        password=password,
-        authSource=database
-    )
-
-    return client[database]
+def get_mongodb_connection(uri: str):
+    client = MongoClient(uri, serverSelectionTimeoutMS=5000)
+    client.admin.command("ping")  # fail fast if unreachable
+    return client.get_default_database()
